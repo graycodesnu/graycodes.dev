146e5ce9913ad041c188e8ece3b88041ca408330
Grayson Harvey <graycodes.nu@gmail.com>
2023-02-05 19:01:43 -0600

    browser router hookup with pages

--- portfolio-gs/src/App.js
+++ portfolio-gs/src/App.js
@@ -1,12 +1,22 @@
 import Navbar from "./components/Navbar";
+import { BrowserRoute, Routes, Route, BrowserRouter} from "react-router-dom";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Projects from "./components/pages/Projects";
+import Contact from "./components/pages/Contact";
 
 function App() {
   return (
-    <div className = "App">
-<Navbar/>
-
-
-
+    <div className="App">
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/projects" element={<Projects/>} />
+          <Route path="/contact" element={<Contact/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
