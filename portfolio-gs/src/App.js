c17209ba84d7ca487ab6f1346e0ec14da61f46c4
Grayson Harvey <graycodes.nu@gmail.com>
2023-02-06 02:12:16 -0600

    test remote branch, footer add and hookup

--- portfolio-gs/src/App.js
+++ portfolio-gs/src/App.js
@@ -4,6 +4,7 @@ import Home from "./components/Home";
 import Background from "./components/Background";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
+import Footer from "./components/footer";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/projects" element={<Projects />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
+        <Footer/>
       </BrowserRouter>
     </div>
   );
