e59129e8053eb061119734743644068427975c8e
Grayson Harvey <graycodes.nu@gmail.com>
2023-02-06 18:58:18 -0600

    footer

--- portfolio-gs/src/components/footer.js
+++ portfolio-gs/src/components/footer.js
@@ -1,5 +1,14 @@
 export default function Footer() {
   return (
-<h1>footer</h1>
+    <footer className="bg-black-variant ">
+      <div className="mx-auto max-w-7xl py-12 px-6 md:flex md:items-center md:justify-between lg:px-8">
+
+        <div className="mt-8 md:order-1 md:mt-0">
+          <p className=" text-xs  text-light-grey">
+           Grayson Harvey  &copy;2023 
+          </p>
+        </div>
+      </div>
+    </footer>
   );
 }
