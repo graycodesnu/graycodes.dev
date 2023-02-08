d2cb923ea7c67738db82f5fe3f3686d9d9fa1d3d
Grayson Harvey <graycodes.nu@gmail.com>
2023-02-07 22:22:21 -0600

    defined and imported hero data

--- portfolio-gs/src/components/Hero.js
+++ portfolio-gs/src/components/Hero.js
@@ -1,7 +1,28 @@
+import { heroData } from "../data/heroData";
 
 export default function Hero() {
 
-return (
-<h1>HOME TEST</h1>
-)
+  // Hero greeting 
+  const heroGreeting = heroData.greeting.length ? (
+    heroData.greeting.map((greeting, index) => <h1 key={index}>{greeting}</h1>)
+  ) : (
+    <p>Data Not Found</p>
+  );
+
+  // Hero bio 
+  const heroBio = heroData.bio.length ? (
+    heroData.bio.map((bio, index) => <h1 key={index}>{bio}</h1>)
+  ) : (
+    <p>Data Not Found</p>
+  );
+
+
+  return (
+  <>
+  <div>
+  <h1>{heroGreeting}</h1>
+  </div>
+  <div><h6>{heroBio}</h6></div>
+</>
+  );
 }
