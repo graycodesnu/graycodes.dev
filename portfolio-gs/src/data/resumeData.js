be1510812db503177323ce29bb31eeb68fda33e6
Grayson Harvey <graycodes.nu@gmail.com>
2023-02-11 23:53:56 -0600

    added resume data

---
+++ portfolio-gs/src/data/resumeData.js
@@ -0,0 +1,5 @@
+const frontend = ["HTML", "CSS", "JavaScript", "React", "Material UI", "jQuery", "Bootstrap"]
+const backend = ["JavaScript", "MySQL", "Express.js", "Node.js", "MongoDB", "Mongoose", "GraphQL", "Python"]
+
+export const proficiencies =
+  { id: 1, frontend, backend }
