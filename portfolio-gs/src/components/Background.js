d7d5e34b1c1a4e354ceae751636189dd4104123a
Grayson Harvey <graycodes.nu@gmail.com>
2023-02-11 23:58:17 -0600

    imported resume data to background.js

--- portfolio-gs/src/components/Background.js
+++ portfolio-gs/src/components/Background.js
@@ -1,4 +1,25 @@
+import { proficiencies } from "../data/resumeData";
+
 export default function Background() {
+
+    // Frontend
+    const frontendList = proficiencies.frontend.length ? (
+      proficiencies.frontend.map((frontend, index) => (
+        <li key={index}>{frontend}</li>
+      ))
+    ) : (
+      <p>No data to show.</p>
+    );
+  
+    // Backend
+    const backendList = proficiencies.backend.length ? (
+      proficiencies.backend.map((backend, index) => (
+        <li key={index}>{backend}</li>
+      ))
+    ) : (
+      <p>No data to show.</p>
+    );
+
 
   return (
   <>  
