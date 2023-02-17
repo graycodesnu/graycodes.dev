31020e85c9e58cf64faf6006e04d4c802ef57b0e
Grayson Harvey <graycodes.nu@gmail.com>
2023-02-16 22:42:40 -0600

    responsive navbar work - resume button and logo with bars menu working

--- portfolio-gs/src/components/Navbar.js
+++ portfolio-gs/src/components/Navbar.js
@@ -3,7 +3,7 @@ import { ReactComponent as Logo } from "../assets/logo.svg";
 // Import responsive elements
 import { Fragment } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
-import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 
 // Nav routes
 const navigation = [
