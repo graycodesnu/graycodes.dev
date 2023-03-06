7f025bb89deac90d8f602d9f00326003ae6962f9
Grayson Harvey <graycodes.nu@gmail.com>
2023-03-06 00:28:40 -0600

    hero animation successful

--- src/components/home/Hero.js
+++ src/components/home/Hero.js
@@ -3,7 +3,6 @@ import { Link } from "react-scroll";
 // Import animation
 import { motion } from "framer-motion";
 // Import data
-import { heroData } from "../../data/heroData";
 import heroImg from "../../assets/images/heroImg.jpg";
 
 // Animation
@@ -17,14 +16,23 @@ const itemVariants = {
 export default function Hero() {
   return (
     <>
-      <div id="hero" className="relative ml-20 mt-0.5">
+      <motion.div
+        id="hero"
+        className="relative ml-20 mt-0.5"
+        initial="hidden"
+        animate="visible"
+        exit={{ opacity: 0, transition: { duration: 1 } }}
+        variants={{
+          visible: { transition: { staggerChildren: 0.3 } },
+        }}
+      >
         <div className="mx-auto max-w-7xl">
           {/* Greeting, bio, link, polygon */}
           <div className="relative z-10 lg:w-full lg:max-w-2xl">
             {/* Greeting, bio, link */}
             <div className="relative py-32 px-6 sm:py-40 lg:py-56 lg:px-8 lg:pr-0">
               {/* Greeting */}
-              <motion.div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-xl">
+              <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-xl">
                 <motion.h1
                   className="pb-1 text-5xl font-bold tracking-tight text-secondary sm:text-6xl heroUnderline"
                   variants={{
@@ -32,19 +40,23 @@ export default function Hero() {
                     visible,
                   }}
                 >
-                  Hi, I'm Grayson.<span className="inline-flex animate-pulse">_</span>
+                  Hi, I'm Grayson.
+                  <span className="inline-flex animate-pulse">_</span>
                 </motion.h1>
-              </motion.div>
+              </div>
 
               {/* Bio */}
-              <motion.div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-xl">
-                <motion.p className="mt-6 text-lg leading-8 text-secondary">
+              <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-xl">
+                <motion.p
+                  className="mt-6 text-lg leading-8 text-secondary"
+                  variants={itemVariants}
+                >
                   I'm a{" "}
                   <span className="underline font-medium">
                     full-stack engineer
                   </span>{" "}
                   with strong skills in{" "}
-                  <span className="underline decoration-blue-light font-medium">
+                  <span className="underline decoration-purple   font-medium">
                     JavaScript
                   </span>
                   ,{" "}
@@ -68,7 +80,10 @@ export default function Hero() {
                   Psychology and International Studies.{" "}
                 </motion.p>
                 {/* Link */}
-                <motion.div className="mt-10 flex items-center gap-x-6">
+                <motion.div
+                  className="mt-10 flex items-center gap-x-6"
+                  variants={itemVariants}
+                >
                   <Link
                     to="projects"
                     spy={true}
@@ -80,31 +95,24 @@ export default function Hero() {
                     View my projects <span aria-hidden="true">→</span>
                   </Link>
                 </motion.div>
-              </motion.div>
+              </div>
             </div>
           </div>
         </div>
 
         {/* Image */}
-        <motion.div
+        <div
           className="
        lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2"
-          initial="hidden"
-          animate="visible"
-          exit={{ opacity: 10, transition: { duration: 2 } }}
-          variants={{ visible: { transition: { staggerChildren: 0.45 } } }}
         >
           <motion.img
             className="aspect-auto object-cover rounded-lg xlg:aspect-auto lg:h-full lg:w-full 2xl:object-contain"
             src={heroImg}
             alt="Grayson Harvey"
-            variants={{
-              hidden: { opacity: -10 },
-              visible,
-            }}
+            variants={itemVariants}
           />
-        </motion.div>
-      </div>
+        </div>
+      </motion.div>
     </>
   );
 }
