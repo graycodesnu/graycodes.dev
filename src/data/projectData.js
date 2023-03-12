e2e199f8cb3d33ba49161c2d1efb27eb25999cf3
Grayson Harvey <graycodes.nu@gmail.com>
2023-03-11 22:02:14 -0600

    actual project data appending

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -2,78 +2,78 @@
 // TODO: The rest of project data
 // TODO: Add svg screenshots for each project
 export const projectData = [
-    { id: 1,
-    title: "Mix"}
+
   //! Mix-n-Match
-  // {
-  //   id: 1,
-  //   title: "Mix-N-Match",
-  //   screenshot: "",
-  //   github: "https://github.com/graycodesnu/mix-match",
-  //   live: "",
-  //   description:
-  //     "A MERN dating app that allows users to create a playlist – or mixtape – that acts as their profile. Users are able to browse other users’ mixtapes and match with them.",
-  //   responsibilities: [
-  //     "Backend API",
-  //     "Models and queries",
-  //     "Authorization/login functionality using JSON web tokens",
-  //     "UI/UX (Material UI)",
-  //   ],
-  //   skills: [
-  //     "React",
-  //     "MongoDB",
-  //     "JavaScript",
-  //     "JSX",
-  //     "GraphQL",
-  //     "Material UI",
-  //     "bcrypt",
-  //     "Express.js",
-  //     "Node.js",
-  //     "Heroku",
-  //   ],
-  // },
-  // //! GoodWeeds
-  // {
-  //   id: 2,
-  //   title: "GoodWeeds",
-  //   screenshot: "",
-  //   github: "https://github.com/graycodesnu/goodWeeds",
-  //   live: "",
-  //   description:
-  //     "Utilizes a MySQL database – built from scratch – to allow a user to create a secure account, browse popular weed strains, add strains to favorites, and post a review to the WeedFeed.",
-  //   responsibilities: [
-  //     "Agile development upon stepping into a leadership role",
-  //     "Backend functionality and database integration with MySQL",
-  //     "Routes and data calls",
-  //     "Login authorization and sign up",
-  //     "UX/UI",
-  //   ],
-  //   skills: [
-  //     "JavaScript",
-  //     "HTML",
-  //     "Handlebars",
-  //     "Bootstrap",
-  //     "CSS",
-  //     "bcrypt",
-  //     "Express.js",
-  //     "Node.js",
-  //     "Heroku",
-  //   ],
-  // },
-  // //! Intertain
-  // {
-  //   id: 3,
-  //   title: "Intertain",
-  //   screenshot: "",
-  //   github: "https://github.com/NW-Bootcamp-Team1/Night-In",
-  //   live: "https://nw-bootcamp-team1.github.io/Night-In/home-page/index",
-  //   description:
-  //     "Utilizes third-party APIs and local storage to allow users to randomly generate entertainment options in the form of a book, movie, and board game, which can be saved in local storage or refreshed.",
-  //   responsibilities: [
-  //     "Local storage functionality",
-  //     "Third-party API and implementation",
-  //     "UX/UI",
-  //   ],
-  //   skills: ["JavaScript", "HTML", "CSS", "Heroku"],
-  // },
+  {
+    id: 1,
+    title: "Mix-N-Match",
+    screenshot: "",
+    github: "https://github.com/graycodesnu/mix-match",
+    live: "",
+    description:
+      "A MERN dating app that allows users to create a playlist – or mixtape – that acts as their profile. Users are able to browse other users’ mixtapes and match with them.",
+    responsibilities: [
+      "Backend API",
+      "Models and queries",
+      "Authorization/login functionality using JSON web tokens",
+      "UI/UX (Material UI)",
+    ],
+    skills: [
+      "React",
+      "MongoDB",
+      "JavaScript",
+      
+      "JSX",
+      "GraphQL",
+      "Material UI",
+      "bcrypt",
+      "Express.js",
+      "Node.js",
+      "Heroku",
+    ],
+  },
+  //! GoodWeeds
+  {
+    id: 2,
+    title: "GoodWeeds",
+    screenshot: "",
+    github: "https://github.com/graycodesnu/goodWeeds",
+    live: "",
+    description:
+      "Utilizes a MySQL database – built from scratch – to allow a user to create a secure account, browse popular weed strains, add strains to favorites, and post a review to the WeedFeed.",
+    responsibilities: [
+      "Agile development upon stepping into a leadership role",
+      "Backend functionality and database integration with MySQL",
+      "Routes and data calls",
+      "Login authorization and sign up",
+      "UX/UI",
+    ],
+    skills: [
+      "JavaScript",
+      "HTML",
+      "Handlebars",
+      "Bootstrap",
+      "CSS",
+      "bcrypt",
+      "Express.js",
+      "Node.js",
+      "Heroku",
+    ],
+  },
+  //! Intertain
+  {
+    id: 3,
+    title: "Intertain",
+    screenshot: "",
+    github: "https://github.com/NW-Bootcamp-Team1/Night-In",
+    live: "https://nw-bootcamp-team1.github.io/Night-In/home-page/index",
+    description:
+      "Utilizes third-party APIs and local storage to allow users to randomly generate entertainment options in the form of a book, movie, and board game, which can be saved in local storage or refreshed.",
+    responsibilities: [
+      "Local storage functionality",
+      "Third-party API and implementation",
+      "UX/UI",
+    ],
+    skills: ["JavaScript", "HTML", "CSS", "Heroku"],
+  },
 ];
