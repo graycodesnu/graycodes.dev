b041b7f708c43b8497175192dfc26f259e2d4065
Grayson Harvey <graycodes.nu@gmail.com>
2023-03-27 23:23:25 -0500

    contact header work

--- src/components/home/Contact.js
+++ src/components/home/Contact.js
@@ -1,32 +1,21 @@
 // Animation
 import { motion } from "framer-motion";
 
-
 const visible = { opacity: 1, y: 0, transition: { duration: 0.75 } };
 
 const itemVariants = {
   hidden: { opacity: 0, y: 10 },
-  visible
+  visible,
 };
 
 export default function Contact() {
   return (
     <>
-      <motion.div id="contact"
-      initial="hidden"
-      animate="visible"
-      exit={{ opacity: 0, transition: { duration: 1 } }}
-      variants={{ visible: { transition: { staggerChildren: 0.3 } } }}
-      >
-      <motion.h1 
-      className="text-6xl text-secondary font-bold p-6"
-      variants={{
-        hidden: { opacity: 0, y: -20 },
-        visible
-      }}
-      >Contact</motion.h1>
-
-      </motion.div>
+      <div id="contact">
+        <h1 className="xl:mt-14 text-5xl font-bold tracking-tight text-secondary sm:text-6xl">
+          <span className="contactHeadingUnderline">Say Hello!</span>
+        </h1>
+      </div>
     </>
   );
 }
