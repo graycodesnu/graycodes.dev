4e35115cdb24960b86a94b9b38949fd2d9474daa
Grayson Harvey <graycodes.nu@gmail.com>
2023-04-04 15:19:59 -0500

    message field format.

--- src/components/home/Contact.js
+++ src/components/home/Contact.js
@@ -268,23 +268,26 @@ export default function Contact() {
                   </div>
 
                   {/* Message */}
-                  <div className="row formRow">
-                    <div className="col">
+                  <div className="sm:col-span-2">
+                    <label className="block text-sm font-semibold leading-6">
+                      Message
+                    </label>
+                    <div className="mt-2.5">
                       <textarea
                         rows={3}
                         name="message"
                         {...register("message", {
                           required: true,
                         })}
-                        className="form-control formInput"
+                        className="form-control formInput block w-full rounded-md border-0 px-3.5 py-2 text-blue shadow-sm ring-1 ring-inset ring-green focus:ring-2 focus:ring-inset focus:ring-green-light sm:text-sm sm:leading-6"
                         placeholder="Message"
-                      ></textarea>
-                      {errors.message && (
-                        <span className="errorMessage">
-                          Please enter a message
-                        </span>
-                      )}
+                      />
                     </div>
+                    {errors.message && (
+                      <span className="errorMessage">
+                        Please include a message.
+                      </span>
+                    )}
                   </div>
                 </div>
                 {/* Submit Button */}
