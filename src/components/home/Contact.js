d05d04fcf02297a1f9002e133ae0705ae295d1ba
Grayson Harvey <graycodes.nu@gmail.com>
2023-04-04 17:21:37 -0500

    contact styling and data

--- src/components/home/Contact.js
+++ src/components/home/Contact.js
@@ -82,44 +82,42 @@ export default function Contact() {
           <div className="mx-auto grid max-w-7xl grid-cols-1 lg:grid-cols-2">
             <div className="relative px-6 pb-20 pt-24 sm:pt-32 lg:static lg:px-8 lg:py-48">
               <div className="mx-auto max-w-xl lg:mx-0 lg:max-w-lg">
-                <div className="absolute inset-y-0 left-0 -z-10 w-full overflow-hidden bg-gray-100 ring-1 ring-gray-900/10 lg:w-1/2"></div>
-                <h2 className="text-3xl font-bold tracking-tight text-secondary">
-                  Get in touch
+                <div className="absolute inset-y-0 left-0 -z-10 w-full overflow-hidden border-r border-green lg:w-1/2"></div>
+                <h2 className="text-5xl font-bold tracking-tight text-secondary">
+                  <span className="contactHeadingUnderline">Say Hello</span>
                 </h2>
-                <p className="mt-6 text-lg leading-8 text-gray-600">
+                <p className="mt-6 text-lg leading-8 text-silver">
                   Proin volutpat consequat porttitor cras nullam gravida at.
                   Orci molestie a eu arcu. Sed ut tincidunt integer elementum id
                   sem. Arcu sed malesuada et magna.
                 </p>
-                <dl className="mt-10 space-y-4 text-base leading-7 text-gray-600">
+                <dl className="mt-10 space-y-4 text-base leading-7">
                   <div className="flex gap-x-4">
                     <dt className="flex-none">
                       <span className="sr-only">Telephone</span>
                       <BuildingOffice2Icon
-                        className="h-7 w-6 text-gray-400"
+                        className="h-7 w-6 text-purple"
                         aria-hidden="true"
                       />
                     </dt>
                     <dd>
-                      545 Mavis Island
-                      <br />
-                      Chicago, IL 99191
+                      Chicago, IL
                     </dd>
                   </div>
                   <div className="flex gap-x-4">
                     <dt className="flex-none">
                       <span className="sr-only">Telephone</span>
                       <PhoneIcon
-                        className="h-7 w-6 text-gray-400"
+                        className="h-7 w-6 text-purple"
                         aria-hidden="true"
                       />
                     </dt>
                     <dd>
                       <a
-                        className="hover:text-gray-900"
+                        className="hover:text-gold"
                         href="tel:+1 (555) 234-5678"
                       >
-                        +1 (555) 234-5678
+                        +1 (714) 733-3371
                       </a>
                     </dd>
                   </div>
@@ -127,16 +125,16 @@ export default function Contact() {
                     <dt className="flex-none">
                       <span className="sr-only">Telephone</span>
                       <EnvelopeIcon
-                        className="h-7 w-6 text-gray-400"
+                        className="h-7 w-6 text-purple"
                         aria-hidden="true"
                       />
                     </dt>
                     <dd>
                       <a
-                        className="hover:text-gray-900"
-                        href="mailto:hello@example.com"
+                        className="hover:text-gold"
+                        href="mailto:graycodes.nu@gmail.com"
                       >
-                        hello@example.com
+                        graycodes.nu@gmail.com
                       </a>
                     </dd>
                   </div>
@@ -295,7 +293,7 @@ export default function Contact() {
                   {/* Submit Button */}
                   <div className="mt-8 flex justify-end">
                     <button
-                      className="submit-btn rounded-md bg-green px-3.5 py-2.5 text-white-light text-center text-sm font-semibold shadow-sm hover:bg-blue hover:text-gold"
+                      className="submit-btn rounded-md outline outline-green px-3.5 py-2.5 text-white-light text-center text-sm font-semibold shadow-sm hover:bg-blue hover:text-gold hover:outline-none"
                       type="submit"
                     >
                       Submit Message
@@ -304,7 +302,6 @@ export default function Contact() {
                 </div>
               </form>
             </div>
-            {/* LARGER DIV */}
           </div>
         </div>
       </div>
