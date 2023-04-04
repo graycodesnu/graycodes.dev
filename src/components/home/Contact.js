cb0f6cae148980c84148b2b08919d5f0e703ec61
Grayson Harvey <graycodes.nu@gmail.com>
2023-04-03 19:03:11 -0500

    replaced tailwind form with working one. working. needs styling.

--- src/components/home/Contact.js
+++ src/components/home/Contact.js
@@ -1,14 +1,18 @@
 // ! Imports
 // Animation
 import { motion } from "framer-motion";
-// State
-import { useState } from "react";
 // Email/Message Send Functionality
 import emailjs from "emailjs-com";
 // Form Element
 import { useForm } from "react-hook-form";
 // Notification
 import { ToastContainer, toast } from "react-toastify";
+// Icons
+import {
+  BuildingOffice2Icon,
+  EnvelopeIcon,
+  PhoneIcon,
+} from "@heroicons/react/24/outline";
 
 // ! Animation
 const visible = { opacity: 1, y: 0, transition: { duration: 0.75 } };
@@ -45,12 +49,13 @@ export default function Contact() {
   // Function called on submit that uses emailjs to send email of valid contact form
   const onSubmit = async (data) => {
     // Destrcture data object
-    const { name, email, subject, message } = data;
+    const { firstName, lastName, email, phone, message } = data;
     try {
       const templateParams = {
-        name,
+        firstName,
+        lastName,
         email,
-        subject,
+        phone,
         message,
       };
 
@@ -72,86 +77,155 @@ export default function Contact() {
   return (
     <>
       <div id="contact">
-        <div className="ContactForm">
-          <div className="container">
-            <div className="row">
-              <div className="">
-                <div className="">
-                  <form
-                    id="contact-form"
-                    onSubmit={handleSubmit(onSubmit)}
-                    noValidate
-                  >
-                    {/* Row 1 of form */}
+        {/* TAILWIND COMPONENT */}
+        <div className="relative isolate">
+          <div className="mx-auto grid max-w-7xl grid-cols-1 lg:grid-cols-2">
+            <div className="relative px-6 pb-20 pt-24 sm:pt-32 lg:static lg:px-8 lg:py-48">
+              <div className="mx-auto max-w-xl lg:mx-0 lg:max-w-lg">
+                <div className="absolute inset-y-0 left-0 -z-10 w-full overflow-hidden bg-gray-100 ring-1 ring-gray-900/10 lg:w-1/2"></div>
+                <h2 className="text-3xl font-bold tracking-tight text-secondary">
+                  Get in touch
+                </h2>
+                <p className="mt-6 text-lg leading-8 text-gray-600">
+                  Proin volutpat consequat porttitor cras nullam gravida at.
+                  Orci molestie a eu arcu. Sed ut tincidunt integer elementum id
+                  sem. Arcu sed malesuada et magna.
+                </p>
+                <dl className="mt-10 space-y-4 text-base leading-7 text-gray-600">
+                  <div className="flex gap-x-4">
+                    <dt className="flex-none">
+                      <span className="sr-only">Telephone</span>
+                      <BuildingOffice2Icon
+                        className="h-7 w-6 text-gray-400"
+                        aria-hidden="true"
+                      />
+                    </dt>
+                    <dd>
+                      545 Mavis Island
+                      <br />
+                      Chicago, IL 99191
+                    </dd>
+                  </div>
+                  <div className="flex gap-x-4">
+                    <dt className="flex-none">
+                      <span className="sr-only">Telephone</span>
+                      <PhoneIcon
+                        className="h-7 w-6 text-gray-400"
+                        aria-hidden="true"
+                      />
+                    </dt>
+                    <dd>
+                      <a
+                        className="hover:text-gray-900"
+                        href="tel:+1 (555) 234-5678"
+                      >
+                        +1 (555) 234-5678
+                      </a>
+                    </dd>
+                  </div>
+                  <div className="flex gap-x-4">
+                    <dt className="flex-none">
+                      <span className="sr-only">Telephone</span>
+                      <EnvelopeIcon
+                        className="h-7 w-6 text-gray-400"
+                        aria-hidden="true"
+                      />
+                    </dt>
+                    <dd>
+                      <a
+                        className="hover:text-gray-900"
+                        href="mailto:hello@example.com"
+                      >
+                        hello@example.com
+                      </a>
+                    </dd>
+                  </div>
+                </dl>
+              </div>
+            </div>
+            {/* ********************* Form ********************* */}
+            {/* WORKING CONTACT FORM */}
+            <div className="ContactForm">
+              <div className="container">
+                <div className="row">
+                  <div className="">
                     <div className="">
-                      <div className="">
-                        <input
-                          type="text"
-                          name="name"
-                          {...register("name", {
-                            required: {
-                              value: true,
-                              message: "Please enter your name",
-                            },
-                            maxLength: {
-                              value: 30,
-                              message: "Please use 30 characters or less",
-                            },
-                          })}
-                          className="form-control formInput"
-                          placeholder="Name"
-                        ></input>
-                        {errors.name && (
-                          <span className="errorMessage">
-                            {errors.name.message}
-                          </span>
-                        )}
-                      </div>
-                      <div className="col-6">
-                        <input
-                          type="email"
-                          name="email"
-                          {...register("email", {
-                            required: true,
-                            pattern:
-                              /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
-                          })}
-                          className="form-control formInput"
-                          placeholder="Email address"
-                        ></input>
-                        {errors.email && (
-                          <span className="errorMessage">
-                            Please enter a valid email address
-                          </span>
-                        )}
-                      </div>
+                      <form
+                        id="contact-form"
+                        onSubmit={handleSubmit(onSubmit)}
+                        noValidate
+                      >
+                        {/* Row 1 of form */}
+                        <div className="">
+                          <div className="">
+                            <input
+                              type="text"
+                              name="firstName"
+                              {...register("firstName", {
+                                required: {
+                                  value: true,
+                                  message: "Please enter your first name.",
+                                },
+                                maxLength: {
+                                  value: 30,
+                                  message: "Please use 30 characters or less",
+                                },
+                              })}
+                              className="form-control formInput"
+                              placeholder="Name"
+                            ></input>
+                            {errors.name && (
+                              <span className="errorMessage">
+                                {errors.name.message}
+                              </span>
+                            )}
+                          </div>
+                          <div className="col-6">
+                            <input
+                              type="email"
+                              name="email"
+                              {...register("email", {
+                                required: true,
+                                pattern:
+                                  /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                              })}
+                              className="form-control formInput"
+                              placeholder="Email address"
+                            ></input>
+                            {errors.email && (
+                              <span className="errorMessage">
+                                Please enter a valid email address
+                              </span>
+                            )}
+                          </div>
+                        </div>
+                        {/* Row 3 of form */}
+                        <div className="row formRow">
+                          <div className="col">
+                            <textarea
+                              rows={3}
+                              name="message"
+                              {...register("message", {
+                                required: true,
+                              })}
+                              className="form-control formInput"
+                              placeholder="Message"
+                            ></textarea>
+                            {errors.message && (
+                              <span className="errorMessage">
+                                Please enter a message
+                              </span>
+                            )}
+                          </div>
+                        </div>
+                        <button className="submit-btn" type="submit">
+                          Submit
+                        </button>
+                      </form>
                     </div>
-
-                    {/* Row 3 of form */}
-                    <div className="row formRow">
-                      <div className="col">
-                        <textarea
-                          rows={3}
-                          name="message"
-                          {...register("message", {
-                            required: true,
-                          })}
-                          className="form-control formInput"
-                          placeholder="Message"
-                        ></textarea>
-                        {errors.message && (
-                          <span className="errorMessage">
-                            Please enter a message
-                          </span>
-                        )}
-                      </div>
-                    </div>
-                    <button className="submit-btn" type="submit">
-                      Submit
-                    </button>
-                  </form>
+                    <ToastContainer />
+                  </div>
                 </div>
-                <ToastContainer />
               </div>
             </div>
           </div>
