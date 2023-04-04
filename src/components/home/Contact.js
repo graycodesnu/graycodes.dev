65a673985cc62b2632635a88980c89216725add4
Grayson Harvey <graycodes.nu@gmail.com>
2023-04-04 16:48:51 -0500

    toastify notification styling

--- src/components/home/Contact.js
+++ src/components/home/Contact.js
@@ -145,162 +145,165 @@ export default function Contact() {
             </div>
             {/* ********************* Form ********************* */}
             {/* DIV WITH FORM AND TOASTIFY NOTIF */}
-            <form
-              id="contact-form"
-              onSubmit={handleSubmit(onSubmit)}
-              noValidate
-              className="px-6 pb-24 pt-20 sm:pb-32 lg:px-8 lg:py-48"
-            >
-              {/* WHOLE FORM DIV */}
-              <div className="mx-auto max-w-xl lg:mr-0 lg:max-w-lg">
-                {/* DIV WITHOUT BUTTON */}
-                <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
-                  {/* First Name */}
-                  <div>
-                    <label className="block text-sm font-semibold leading-6">
-                      First Name
-                    </label>
-                    <div className="mt-2.5">
-                      <input
-                        type="text"
-                        name="firstName"
-                        {...register("firstName", {
-                          required: {
-                            value: true,
-                            message: "Please enter your first name.",
-                          },
-                          maxLength: {
-                            value: 30,
-                            message: "Please use 30 characters or less",
-                          },
-                        })}
-                        className="form-control formInput block w-full rounded-md border-0 px-3.5 py-2 text-blue shadow-sm ring-1 ring-inset ring-green focus:ring-2 focus:ring-inset focus:ring-green-light sm:text-sm sm:leading-6"
-                        placeholder="First Name"
-                      />
+            <div className="px-6 pb-24 pt-20 sm:pb-32 lg:px-8 lg:py-48">
+              <form
+                id="contact-form"
+                onSubmit={handleSubmit(onSubmit)}
+                noValidate
+              >
+                {/* WHOLE FORM DIV */}
+                <div className="mx-auto max-w-xl lg:mr-0 lg:max-w-lg">
+                  {/* DIV WITHOUT BUTTON */}
+                  <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+                    {/* First Name */}
+                    <div>
+                      <label className="block text-sm font-semibold leading-6">
+                        First Name
+                      </label>
+                      <div className="mt-2.5">
+                        <input
+                          type="text"
+                          name="firstName"
+                          {...register("firstName", {
+                            required: {
+                              value: true,
+                              message: "Please enter your first name.",
+                            },
+                            maxLength: {
+                              value: 30,
+                              message: "Please use 30 characters or less",
+                            },
+                          })}
+                          className="form-control formInput block w-full rounded-md border-0 px-3.5 py-2 text-blue shadow-sm ring-1 ring-inset ring-green focus:ring-2 focus:ring-inset focus:ring-green-light sm:text-sm sm:leading-6"
+                          placeholder="First Name"
+                        />
+                      </div>
+                      {errors.firstName && (
+                        <span className="errorMessage text-red-light">
+                          {errors.firstName.message}
+                        </span>
+                      )}
                     </div>
-                    {errors.firstName && (
-                      <span className="errorMessage text-red-light">
-                        {errors.firstName.message}
-                      </span>
-                    )}
-                  </div>
-                  {/* Last Name */}
-                  <div>
-                    <label className="block text-sm font-semibold leading-6">
-                      Last Name
-                    </label>
-                    <div className="mt-2.5">
-                      <input
-                        type="text"
-                        name="lastName"
-                        {...register("lastName", {
-                          required: {
-                            value: true,
-                            message: "Please enter your last name.",
-                          },
-                          maxLength: {
-                            value: 30,
-                            message: "Please use 30 characters or less",
-                          },
-                        })}
-                        className="form-control formInput block w-full rounded-md border-0 px-3.5 py-2 text-blue shadow-sm ring-1 ring-inset ring-green focus:ring-2 focus:ring-inset focus:ring-green-light sm:text-sm sm:leading-6"
-                        placeholder="Last Name"
-                      />
+                    {/* Last Name */}
+                    <div>
+                      <label className="block text-sm font-semibold leading-6">
+                        Last Name
+                      </label>
+                      <div className="mt-2.5">
+                        <input
+                          type="text"
+                          name="lastName"
+                          {...register("lastName", {
+                            required: {
+                              value: true,
+                              message: "Please enter your last name.",
+                            },
+                            maxLength: {
+                              value: 30,
+                              message: "Please use 30 characters or less",
+                            },
+                          })}
+                          className="form-control formInput block w-full rounded-md border-0 px-3.5 py-2 text-blue shadow-sm ring-1 ring-inset ring-green focus:ring-2 focus:ring-inset focus:ring-green-light sm:text-sm sm:leading-6"
+                          placeholder="Last Name"
+                        />
+                      </div>
+                      {errors.lastName && (
+                        <span className="errorMessage text-red-light">
+                          {errors.lastName.message}
+                        </span>
+                      )}
                     </div>
-                    {errors.lastName && (
-                      <span className="errorMessage text-red-light">
-                        {errors.lastName.message}
-                      </span>
-                    )}
-                  </div>
-                  {/* Email */}
-                  <div className="sm:col-span-2">
-                    <label className="block text-sm font-semibold leading-6">
-                      Email
-                    </label>
-                    <div className="mt-2.5">
-                      <input
-                        type="email"
-                        name="email"
-                        {...register("email", {
-                          required: true,
-                          pattern:
-                            /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
-                        })}
-                        className="form-control formInput block w-full rounded-md border-0 px-3.5 py-2 text-blue shadow-sm ring-1 ring-inset ring-green focus:ring-2 focus:ring-inset focus:ring-green-light sm:text-sm sm:leading-6"
-                        placeholder="Email Address"
-                      />
+                    {/* Email */}
+                    <div className="sm:col-span-2">
+                      <label className="block text-sm font-semibold leading-6">
+                        Email
+                      </label>
+                      <div className="mt-2.5">
+                        <input
+                          type="email"
+                          name="email"
+                          {...register("email", {
+                            required: true,
+                            pattern:
+                              /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                          })}
+                          className="form-control formInput block w-full rounded-md border-0 px-3.5 py-2 text-blue shadow-sm ring-1 ring-inset ring-green focus:ring-2 focus:ring-inset focus:ring-green-light sm:text-sm sm:leading-6"
+                          placeholder="Email Address"
+                        />
+                      </div>
+                      {errors.email && (
+                        <span className="errorMessage text-red-light">
+                          Please enter a valid email address.
+                        </span>
+                      )}
                     </div>
-                    {errors.email && (
-                      <span className="errorMessage text-red-light">
-                        Please enter a valid email address.
-                      </span>
-                    )}
-                  </div>
-                  {/* Phone */}
-                  <div  className="sm:col-span-2">
-                    <label className="block text-sm font-semibold leading-6">
-                      Phone
-                    </label>
-                    <div className="mt-2.5">
-                      <input
-                        type="text"
-                        name="phone"
-                        {...register("phone", {
-                          required: {
-                            value: true,
-                            message: "Please enter your phone number.",
-                          },
-                          maxLength: {
-                            value: 11,
-                            message: "Please use 11 characters or less",
-                          },
-                        })}
-                        className="form-control formInput block w-full rounded-md border-0 px-3.5 py-2 text-blue shadow-sm ring-1 ring-inset ring-green focus:ring-2 focus:ring-inset focus:ring-green-light sm:text-sm sm:leading-6"
-                        placeholder="Phone Number"
-                      />
+                    {/* Phone */}
+                    <div className="sm:col-span-2">
+                      <label className="block text-sm font-semibold leading-6">
+                        Phone
+                      </label>
+                      <div className="mt-2.5">
+                        <input
+                          type="text"
+                          name="phone"
+                          {...register("phone", {
+                            required: {
+                              value: true,
+                              message: "Please enter your phone number.",
+                            },
+                            maxLength: {
+                              value: 11,
+                              message: "Please use 11 characters or less",
+                            },
+                          })}
+                          className="form-control formInput block w-full rounded-md border-0 px-3.5 py-2 text-blue shadow-sm ring-1 ring-inset ring-green focus:ring-2 focus:ring-inset focus:ring-green-light sm:text-sm sm:leading-6"
+                          placeholder="Phone Number"
+                        />
+                      </div>
+                      {errors.phone && (
+                        <span className="errorMessage text-red-light">
+                          {errors.phone.message}
+                        </span>
+                      )}
                     </div>
-                    {errors.phone && (
-                      <span className="errorMessage text-red-light">
-                        {errors.phone.message}
-                      </span>
-                    )}
-                  </div>
 
-                  {/* Message */}
-                  <div className="sm:col-span-2">
-                    <label className="block text-sm font-semibold leading-6">
-                      Message
-                    </label>
-                    <div className="mt-2.5">
-                      <textarea
-                        rows={3}
-                        name="message"
-                        {...register("message", {
-                          required: true,
-                        })}
-                        className="form-control formInput block w-full rounded-md border-0 px-3.5 py-2 text-blue shadow-sm ring-1 ring-inset ring-green focus:ring-2 focus:ring-inset focus:ring-green-light sm:text-sm sm:leading-6"
-                        placeholder="Message"
-                      />
+                    {/* Message */}
+                    <div className="sm:col-span-2">
+                      <label className="block text-sm font-semibold leading-6">
+                        Message
+                      </label>
+                      <div className="mt-2.5">
+                        <textarea
+                          rows={3}
+                          name="message"
+                          {...register("message", {
+                            required: true,
+                          })}
+                          className="form-control formInput block w-full rounded-md border-0 px-3.5 py-2 text-blue shadow-sm ring-1 ring-inset ring-green focus:ring-2 focus:ring-inset focus:ring-green-light sm:text-sm sm:leading-6"
+                          placeholder="Message"
+                        />
+                      </div>
+                      {errors.message && (
+                        <span className="errorMessage text-red-light">
+                          Please include a message.
+                        </span>
+                      )}
                     </div>
-                    {errors.message && (
-                      <span className="errorMessage text-red-light">
-                        Please include a message.
-                      </span>
-                    )}
+                  </div>
+                  {/* Toastify Notification */}
+                  <ToastContainer closeButton={false}/>
+                  {/* Submit Button */}
+                  <div className="mt-8 flex justify-end">
+                    <button
+                      className="submit-btn rounded-md bg-green px-3.5 py-2.5 text-white-light text-center text-sm font-semibold shadow-sm hover:bg-blue hover:text-gold"
+                      type="submit"
+                    >
+                      Submit Message
+                    </button>
                   </div>
                 </div>
-                {/* Submit Button */}
-                <div className="mt-8 flex justify-end">
-                <button className="submit-btn rounded-md bg-green px-3.5 py-2.5 text-center text-sm font-semibold shadow-sm hover:bg-blue hover:text-gold" type="submit">
-                  Submit Message
-                </button>
-                </div>
-              </div>
-            </form>
-            {/* Toastify Notification */}
-            <ToastContainer />
-
+              </form>
+            </div>
             {/* LARGER DIV */}
           </div>
         </div>
