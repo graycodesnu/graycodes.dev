febbfe84f0c1a075df9d76645f4007c198fbd318
Grayson Harvey <graycodes.nu@gmail.com>
2023-04-10 23:02:24 -0500

    centered card and added display responsiveness for logo

--- src/components/home/InProgressProjectList.js
+++ src/components/home/InProgressProjectList.js
@@ -11,27 +11,6 @@ import {
 // Import screenshot
 import { ReactComponent as MyPetsLogo } from "../../assets/project-screenshots/misc/my-pets-logo.svg";
 
-const features = [
-  {
-    name: "Push to deploy.",
-    description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
-    icon: CloudArrowUpIcon,
-  },
-  {
-    name: "SSL certificates.",
-    description:
-      "Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.",
-    icon: LockClosedIcon,
-  },
-  {
-    name: "Database backups.",
-    description:
-      "Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.",
-    icon: ServerIcon,
-  },
-];
-
 // ! Function
 export default function InProgressProject({ inProgressProject }) {
   // * Short skill array for project preview card
@@ -44,33 +23,16 @@ export default function InProgressProject({ inProgressProject }) {
   // ** Return **
   return (
     <>
-      <div>
-        {/* Heading */}
-        <div>
-          <h2 className="text-secondary text-5xl font-medium flex">
-            <span className="inProgressProjectUnderline">In</span>
-          </h2>
-          <h2 className="text-secondary text-5xl font-medium flex">
-            <span className="inProgressProjectUnderline">Progress</span>
-          </h2>
-        </div>
-        {/* Project Info Section */}
-        <div>
-          {/* Title */}
-          <span className="inline-flex mt-4 min-w-full">
-            <h3 className="text-secondary font-semibold text-2xl">
-              {inProgressProject.title}
-            </h3>
-            <GitHubIcon className="h-8 w-8 text-green" />
-          </span>
-        </div>
-      </div>
-{/* Tailwind Div */}
       <div className="overflow-hidden py-24 sm:py-32">
-        <div className="mx-auto max-w-7xl md:px-6 lg:px-8">
-          <div className="grid grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:grid-cols-2 lg:items-start">
+        {/* Section Title */}
+        <h2 className="text-secondary text-5xl font-medium flex justify-center mb-12">
+          <span className="inProgressProjectUnderline">In Progress</span>
+        </h2>
+        {/* Project Section */}
+        <div className="mx-auto max-w-7xl md:px-6 lg:px-8  flex justify-center">
+          <div className="grid grid-cols-1 gap-x-8 gap-y-16 ml-20 sm:gap-y-20 lg:grid-cols-2 lg:items-start">
             <div className="px-6 md:px-0 lg:pr-4 lg:pt-4">
-              {/* Project Info */}
+              {/* Inner Project Info */}
               <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-lg">
                 {/* Title */}
                 <h3 className="mt-2 text-3xl font-bold tracking-tight text-secondary sm:text-4xl">
@@ -92,45 +54,11 @@ export default function InProgressProject({ inProgressProject }) {
                     </div>
                   ))}
                 </span>
-                {/* <dl className="mt-10 max-w-xl space-y-8 text-base leading-7 text-gold lg:max-w-none">
-                  <div key={inProgressProject.description} className="relative pl-9">
-                    <dt className="inline font-semibold text-green">
-                      <feature.icon className="absolute left-1 top-1 h-5 w-5 text-indigo-600" aria-hidden="true" />
-                      {inProgressProject.description}
-                    </dt>{' '}
-                    <dd className="inline">{feature.description}</dd>
-                  </div>
-              </dl> */}
               </div>
             </div>
             {/* Logo */}
-            <div className="sm:px-6 lg:px-0">
-              {/* <div className="relative isolate overflow-hidden bg-primary-light px-6 py-8 sm:mx-auto sm:max-w-2xl sm:rounded-3xl sm:pl-16 sm:pr-0 sm:pt-16 lg:mx-0 lg:max-w-none"> */}
-
-              <MyPetsLogo className="h-72 w-72" />
-
-              {/* <div
-                className="absolute -inset-y-px -left-3 -z-10 w-full origin-bottom-left skew-x-[-30deg] bg-indigo-100 opacity-20 ring-1 ring-inset ring-white"
-                aria-hidden="true"
-              />
-              <div className="mx-auto max-w-2xl sm:mx-0 sm:max-w-none">
-                <div className="w-screen overflow-hidden rounded-tl-xl bg-red ring-1 ring-gold">
-                  <div className="flex bg-gray-800/40 ring-1 ring-white/5">
-                    <div className="-mb-px flex text-sm font-medium leading-6 text-gray-400">
-                      <div className="border-b border-r border-b-white/20 border-r-white/10 bg-white/5 px-4 py-2 text-white">
-                        NotificationSetting.jsx
-                      </div>
-                      <div className="border-r border-gray-600/10 px-4 py-2">App.jsx</div>
-                    </div>
-                  </div>
-                  <div className="px-6 pb-14 pt-6">Blah blah blah </div>
-                </div>
-              </div>
-              <div
-                className="pointer-events-none absolute inset-0 ring-1 ring-inset ring-black/10 sm:rounded-3xl"
-                aria-hidden="true"
-              /> */}
-              {/* </div> */}
+            <div className="hidden lg:flex mt-12">
+              <MyPetsLogo className="h-72" />
             </div>
           </div>
         </div>
