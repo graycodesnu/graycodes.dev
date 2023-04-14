ce122b40e8603a1501f6297dd682e3068e3ab5c2
Grayson Harvey <graycodes.nu@gmail.com>
2023-04-14 00:23:40 -0500

    animated divider

--- src/components/static/Divider.js
+++ src/components/static/Divider.js
@@ -1,18 +1,44 @@
+// ! Imports
+// Icon
 import { CodeBracketIcon } from "@heroicons/react/24/outline";
+// Animation
+import { motion } from "framer-motion";
 
+// ! Function
 export default function Divider() {
+  // * Animation
+  const visible = {
+    opacity: 1,
+    y: 0,
+    transition: { duration: 1, delay: 0.5 },
+  };
+  const itemVariants = {
+    hidden: { opacity: 0, y: 10 },
+    visible,
+  };
+
+  // * ** RETURN **
   return (
     <>
-      <div className="relative py-10">
+      <motion.div
+        className="relative py-10"
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true }}
+        exit={{ opacity: 0, transition: { duration: 1 } }}
+        variants={{
+          visible: { transition: { staggerChildren: 3 } },
+        }}
+      >
         {/* Gradient Divider */}
-        <div className="absolute inset-0 flex items-center">
+        <motion.div className="absolute inset-0 flex items-center" variants={itemVariants}>
           <div className="w-full h-0.5 bg-gradient-to-r from-blue via-primary to-blue"></div>
-        </div>
+        </motion.div>
         {/* Bracket Icon */}
-        <div className="relative flex justify-center">
+        <motion.div className="relative flex justify-center" variants={itemVariants}>
           <CodeBracketIcon className="inline-flex items-center rounded-full w-6 h-6 bg-primary text-blue" />
-        </div>
-      </div>
+        </motion.div>
+      </motion.div>
     </>
   );
 }
