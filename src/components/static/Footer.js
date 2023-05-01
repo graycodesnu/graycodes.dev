61c0a47e025fb598227da238f35b6ac9d9c7d477
Grayson Harvey <graycodes.nu@gmail.com>
2023-04-30 23:05:31 -0500

    updated footer padding

--- src/components/static/Footer.js
+++ src/components/static/Footer.js
@@ -1,10 +1,10 @@
 export default function Footer() {
   return (
     <footer className="bg-primary-alt bottom-0">
-      <div className="mx-auto  max-w-7xl py-12 px-6 md:flex md:items-center md:justify-between lg:px-8">
+      <div className="mx-auto max-w-7xl py-12 px-16 md:flex md:items-center md:justify-between lg:px-8 border">
 
         <div className="mt-8 md:order-1 md:mt-0">
-          <p className=" text-xs  text-secondary">
+          <p className=" text-xs text-secondary">
            Designed and built by Grayson Harvey  &copy;2023 
           </p>
         </div>
